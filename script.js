075672e22aa9b7cb0ece6567afd498dfc7f8f377
cari <anhthuchevu@gmail.com>
2026-05-04 16:39:06 +0700

    add currency exchange rate table and first version

--- script.js
+++ script.js
@@ -7,3 +7,22 @@
 */
 
 //AUD, EUR, VND, USD
+
+const exchangeRates = {
+    USD: 1,
+    VND: 25400,
+    EUR: 0.92,
+    AUD: 1.51
+};
+
+function convertCurrency(amount, from, to) {
+    if (!exchangeRates[from] || !exchangeRates[to]) {
+        return null;
+    }
+    const amountInUSD = amount / exchangeRates[from];
+    const finalAmount = amountInUSD * exchangeRates[to];
+
+    return finalAmount;
+}
+
+console.log(convertCurrency(100, 'EUR', 'VND'));
